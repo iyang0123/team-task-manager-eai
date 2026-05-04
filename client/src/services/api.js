50e103ec50a89ffe7a73d5d1577aacbeb9e1afc8
taniya <taniyagupta8210@gmail.com>
2026-05-04 12:42:18 +0530

    Connected frontend to live backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://team-task-manager-api-8ek7.onrender.com/api",
+});
+
+API.interceptors.request.use((req) => {
+
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
 });
 
 export default API;
